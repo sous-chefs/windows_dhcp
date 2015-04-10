b9d345806dea2a430fa332a0289cda06f3cfbd80
Derek Groh <dgroh@arch.tamu.edu>
2015-04-10 10:20:01 -0500

    reservation provider - create

---
+++ resources/reservation.rb
@@ -0,0 +1,65 @@
+#
+# Cookbook Name:: windows_dhcp
+# Resource:: reservation
+#
+# Copyright 2014, Texas A&M
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+actions :create, :delete, :update
+default_action :create
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :scopeid, :kind_of => String, :regex => (Resolv::IPv4::Regex or Resolv::IPv6::Regex), :required => true
+attribute :ipaddress, :kind_of => String, :regex => (Resolv::IPv4::Regex or Resolv::IPv6::Regex), :required => true
+attribute :macaddress, :kind_of => String, :required => true
+attribute :computername, :kind_of => String
+attribute :description, :kind_of => String
+attribute :version, :kind_of => String, :default => '4'
+
+#  Optional params shared
+#    AsJob
+#    CimSession
+#    passthru: string
+#    ThrottleLimit: int32
+#    Type: string
+
+
+
+#  Optional params IPv4
+#    ActivatePolicies: Boolean, default true;
+#    Delay: int16;
+#    LeaseDuration: day.hrs:mins:secs; 
+#    MaxBootpClients: int32;
+#    NapEnable:
+#    NapProfile:
+#    SuperScoeName: String
+#    Type: String
+
+#  Optional params IPv6
+#    PassThru
+#    Preference: int16
+#    PreferenceLifeTime: 
+#    Prefix: IPAddress
+#    T1:
+#    T2:
+#    ValidLifeTime: 
+
